af248e9a9025e35df82b0b17b91ad0b748e452e6
sooyeun-kim <sueyeon777@gmail.com>
2024-02-06 18:27:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Login from './pages/user/Login';
+import Error from './pages/Error';
+import Main from './pages/main/Main'
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        {/* <Route path="/" element={ <Layout/> }>
+          <Route index element={ <Main/> }/>  
+
+        </Route> */}
+
+        <Route path="/main" element={ <Main /> } />
+
+        <Route path="/login" element={ <Login/> } />
+        <Route path="*" element={ <Error/> }/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
