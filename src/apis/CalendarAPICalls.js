aa45aa7d2d734b96650f20b6ea3af4bb1a170cb1
CHUWENCHING <chuuu3619@gmail.com>
2024-02-15 20:03:55 +0900

    calendarapi 수정

--- src/apis/CalendarAPICalls.js
+++ src/apis/CalendarAPICalls.js
@@ -1,8 +1,6 @@
 import {
     GET_CALENDAR
 } from '../modules/CalendarModule.js';
-
-
 export const callCalendarListAPI = ({ userCode }) => { 
     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}/calendar/calendarsByUserCode/${ userCode }`;
     
@@ -20,3 +18,23 @@ export const callCalendarListAPI = ({ userCode }) => {
       dispatch({ type: GET_CALENDAR, payload: result});
     };
   };
+
+// export const callCalendarListAPI = ({ userCode }) => { 
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}/calendar/calendarsByUserCode/${ userCode }`;
+    
+//     return async (dispatch, getState) => {
+//         const result = await fetch( requestURL, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Accept: '*/*',
+//                 Authorization: 'Bearer ' + window.localStorage.getItem('accessToken'),
+//             },
+//         }).then((response) => response.json());
+
+//             console.log('[CalendarAPICalls] callProjectDetailAPI RESULT : ',
+//          result);
+//          dispatch({ type: GET_CALENDAR, payload: result.data});
+            
+//         };
+// }
