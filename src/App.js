2c1bf8087515220d0bacf2c31da61da3140bbf96
jjangjjaeyoung <wkdrkwodud2174@gmail.com>
2024-02-22 18:55:05 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -5,28 +5,24 @@ import Error from "./pages/Error";
 import Main from "./pages/main/Main";
 import AddDetailSchedule from "./pages/calendar/cal_addsch_detail";
 import Message from "./pages/message/Message";
-import Group from './pages/group/Group';
-import GeneralDraft from './pages/approval/GeneralDraft';
-import ApprovalMain from './pages/approval/ApprovalMain';
-import BusinessTrip from './pages/approval/BusinessTrip';
-import Education from './pages/approval/Education';
-import GeneraldraftForm from './pages/approval/GeneraldraftForm';
-import EducationForm from './pages/approval/EducationForm';
-import VacationForm from './pages/approval/VacationForm';
-import BusinesstripForm from './pages/approval/BusinesstripForm';
-import SharedInBox from './pages/approval/SharedInBox';
-import InBox from './pages/approval/InBox';
-import Approvaling from './pages/approval/Approvaling';
-import NewApproval from './pages/approval/NewApproval';
-import Vacation from './pages/approval/Vacation';
-import SaveInBox from './pages/approval/SaveInBox';
-import Layout from './layouts/SubLayout';
+import Group from "./pages/group/Group";
+import GeneralDraft from "./pages/approval/GeneralDraft";
+import ApprovalMain from "./pages/approval/ApprovalMain";
+import BusinessTrip from "./pages/approval/BusinessTrip";
+import Education from "./pages/approval/Education";
+import GeneraldraftForm from "./pages/approval/GeneraldraftForm";
+import EducationForm from "./pages/approval/EducationForm";
+import VacationForm from "./pages/approval/VacationForm";
+import BusinesstripForm from "./pages/approval/BusinesstripForm";
+import SharedInBox from "./pages/approval/SharedInBox";
+import Approvaling from "./pages/approval/Approvaling";
+import NewApproval from "./pages/approval/NewApproval";
+import Vacation from "./pages/approval/Vacation";
+import SaveInBox from "./pages/approval/SaveInBox";
+import Layout from "./layouts/SubLayout";
 // import Group from './pages/group/Group';
 
 import ProjectDetail from "./pages/project/project_detail";
-import PersonalContact from "./pages/contact/personalContactList";
-import CompanyContact from "./pages/contact/companyContactList";
-
 import CalendarMainPage from "./pages/calendar/CalendarMain";
 import CalendarSetting from "./pages/calendar/CalendarSetting";
 import ProjectMain from "./pages/project/projectMain";
@@ -38,10 +34,6 @@ import ReservationApply from "./pages/Reservation/ReservationApply";
 import ReservationModify from "./pages/Reservation/ReservationModify";
 import SearchPwd from "./pages/user/SearchPwd";
 import SearchPwdEmail from "./pages/user/SearchPwdEmail";
-import UserList from "./pages/admin/UserList";
-import MyPage from "./pages/user/MyPage";
-import UserRegist from "./pages/admin/UserRegist";
-import ModifyUser from "./pages/admin/ModifyUser";
 
 function App() {
   return (
@@ -52,41 +44,44 @@ function App() {
         <Route path="/searchpwd" element={<SearchPwd />} />
         <Route path="/searchpwdemail" element={<SearchPwdEmail />} />
 
-{/* 
-      <Route path="/main" element={ <Layout/> }>
-        <Route index element={ <Main /> } />
-      </Route> */}
-        <Route path="/login" element={ <Login/> } />
-        <Route path="*" element={ <Error/> }/>
+        <Route path="/" element={<Sublayout />}>
+          {/*메인 전용 헤더 붙히기 전  */}
+          <Route path="/main" element={<Main />} />
+          {/* 쪽지 */}
+          <Route path="/message" element={<Message />} />
 
-        
+          {/* 결재 */}
 
+          <Route path="/generaldraft" element={<GeneralDraft />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/businesstrip" element={<BusinessTrip />} />
+          <Route path="/saveinbox" element={<SaveInBox />} />
+          <Route path="/vacation" element={<Vacation />} />
+          <Route path="/approvalmain" element={<ApprovalMain />} />
+          <Route path="/sharedinbox" element={<SharedInBox />} />
+          <Route path="/approvaling" element={<Approvaling />} />
+          <Route path="/newapproval" element={<NewApproval />} />
+          <Route path="/generaldraftform" element={<GeneraldraftForm />} />
+          <Route path="/educationform" element={<EducationForm />} />
+          <Route path="/businesstripform" element={<BusinesstripForm />} />
+          <Route path="/vacationform" element={<VacationForm />} />
+          <Route path="/group" element={<Group />} />
 
+          {/* 자원예약 */}
 
-        <Route path="/" element={ <Sublayout/> }>
-        <Route path="/reservationmain" element={ <ReservationMain/> } />
-          <Route path="message" element={ <Message/> } />
-          <Route path="/generaldraft" element={ <GeneralDraft/> } />
-          <Route path="/education" element={ <Education/> } />
-          <Route path="/businesstrip" element={ <BusinessTrip/> } />
-          <Route path="/saveinbox" element={ <SaveInBox/> } />
-          <Route path="/vacation" element={ <Vacation/> } />
-          <Route path="/approvalmain" element={ <ApprovalMain/> } />
-          <Route path="/sharedinbox" element={ <SharedInBox/> } />
-          <Route path="/inbox" element={ <InBox/> } />
-          <Route path="/approvaling" element={ <Approvaling/> } />
-          <Route path="/newapproval" element={ <NewApproval/> } />
-          <Route path="/generaldraftform" element={ <GeneraldraftForm/> } />
-          <Route path="/educationform" element={ <EducationForm/> } />
-          <Route path="/businesstripform" element={ <BusinesstripForm/> } />
-          <Route path="/vacationform" element={ <VacationForm/> } />
-          <Route path="/group" element={ <Group/> } />
+          <Route path="/reservationdetails" element={<ReservationDetails />} />
+          <Route path="/reservationapply" element={<ReservationApply />} />
+          <Route path="/reservationmain" element={<ReservationMain />} />
+          <Route path="/reservationmodify" element={<ReservationModify />} />
 
-          <Route path="/project/:proNo" element={ <ProjectDetail/> }/>       
-          <Route path="/schedule/add/detail" element={ <AddDetailSchedule/> }/>
-          <Route path="/calendar" element={ <CalendarMainPage/> } />
-          <Route path="/calendar/setting" element={ <CalendarSetting/>}/>
-          <Route path='/project/main' element={ <ProjectMain/>}/>
+          {/*일정관리- 캘린더, 프로젝트  */}
+
+          <Route path="/project/main" element={<ProjectMain />} />
+          <Route path="/project/:proNo" element={<ProjectDetail />} />
+          <Route path="/calendar" element={<CalendarMainPage />} />
+          <Route path="/calendar/setting" element={<CalendarSetting />} />
+          <Route path="/schedule/add/detail" element={<AddDetailSchedule />} />
+          <Route path="/schedule/edit/:schNo" element={<EditSchedule />} />
         </Route>
       </Routes>
     </BrowserRouter>
