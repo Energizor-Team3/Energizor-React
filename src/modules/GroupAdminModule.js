716d5f38993cf71efee51a3ac3f76b03d05447e1
jiseon <jiseonu650@naver.com>
2024-02-24 17:21:19 +0900

    2024 02 24

--- src/modules/GroupAdminModule.js
+++ src/modules/GroupAdminModule.js
@@ -8,13 +8,15 @@ export const POST_DEPT_INSERT  = 'group/POST_DEPT_INSERT';
 export const POST_TEAM_INSERT  = 'group/POST_TEAM_INSERT';
 export const POST_DEPT_UPDATE = 'group/POST_DEPT_UPDATE';
 export const POST_TEAM_UPDATE = 'group/POST_TEAM_UPDATE';
+export const POST_DEPT_DELETE = 'group/POST_DEPT_DELETE';
 
 
 const actions = createActions({
     [POST_DEPT_INSERT]: () => {},
     [POST_TEAM_INSERT]: () => {},
     [POST_DEPT_UPDATE]: () => {},
-    [POST_TEAM_UPDATE]: () => {}
+    [POST_TEAM_UPDATE]: () => {},
+    [POST_DEPT_DELETE]: () => {},
 
     
 });
@@ -37,6 +39,10 @@ const groupAdminReducer = handleActions(
             return payload;
         },
         [POST_TEAM_UPDATE]: (state, { payload }) => {
+            
+            return payload;
+        },
+        [POST_DEPT_DELETE]: (state, { payload }) => {
             
             return payload;
         }
