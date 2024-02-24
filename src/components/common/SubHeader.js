7f106d0ad32c9f331f3fc455cf3ee284e970e236
jiseon <jiseonu650@naver.com>
2024-02-24 18:26:21 +0900

    2024 02 24

--- src/components/common/SubHeader.js
+++ src/components/common/SubHeader.js
@@ -42,64 +42,70 @@ function Header() {
             <nav>
                 <div id="main_list_icon">
                     <div>
-                    <a href="/group">
-                        <img src="/common/Home.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>홈</span>
+                        <a href="/main">
+                            <img src="/common/Home.png" alt="" />
+                            <span style={spanStyle}>홈</span>
+                        </a>
                     </div>
                     <div>
-                    <a href="/group">
-                        <img src="/common/Approval.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>전자결재</span>
+                        <a href="/approvalmain">
+                            <img src="/common/Approval.png" alt="" />
+                            <span style={spanStyle}>전자결재</span>
+                        </a>
                     </div>
                     <div>
-                    <a href="/calendar">
-                        <img src="/common/Attendance.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>근태관리</span>
+                        <a href="/calendar">
+                            <img src="/common/Attendance.png" alt="" />
+                            <span style={spanStyle}>근태관리</span>
+                        </a>
                     </div>
                     <div>
                     <NavLink to='/calendar'>
                         <img src="/common/calendar.png" alt="캘린더 아이콘" />
-                       <span style={spanStyle}>일정관리</span>
+                        <span style={spanStyle}>일정관리</span>
                     </NavLink>
                     </div>
                     <div>
-                    <a href="/group">
-                        <img src="/common/Address.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>주소록</span>
+                        <a href="/contact/company-list">
+                            <img src="/common/Address.png" alt="" />
+                            <span style={spanStyle}>주소록</span>
+                        </a>
                     </div>
                     <div>
-                    <a href="/group">
-                        <img src="/common/Organization.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>조직도</span>
+                        <a href="/group">
+                            <img src="/common/Organization.png" alt="" />
+                            <span style={spanStyle}>조직도</span>
+                        </a>
                     </div>
                     <div>
-                    <a href="/message">
-                        <img src="/common/Mail.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>쪽지</span>
+                        <a href="/message">
+                            <img src="/common/Mail.png" alt="" />
+                            <span style={spanStyle}>쪽지</span>
+                        </a>
                     </div>
-                    <div>
+                    {/* <div>
                     <a href="/group">
                         <img src="/common/Messanger.png" alt="" />
                     </a>
                     <span style={spanStyle}>메신저</span>
+                    </div> */}
+                    <div>
+                        <a href="/reservationmain">
+                            <img src="/common/reservation.png" alt="" />
+                            <span style={spanStyle}>자원예약</span>
+                        </a>
                     </div>
                     <div>
-                    <a href="/group">
-                        <img src="/common/reservation.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>자원예약</span>
+                        <a href="/board">
+                            <img src="/common/board.png" alt="" />
+                            <span style={spanStyle}>게시판</span>
+                        </a>
                     </div>
                     <div>
-                    <a href="/group">
-                        <img src="/common/board.png" alt="" />
-                    </a>
-                    <span style={spanStyle}>게시판</span>
+                        <a href="/userlist">
+                            <img src="/mypage/hrm.png" alt="" />
+                            <span style={spanStyle}>인사관리</span>
+                        </a>
                     </div>
                 </div>
             </nav>
