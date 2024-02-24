cc5250daaa5ab8d6b251f4f52cd15b18587a925e
kim dahye <rlaekgp331299@gmail.com>
2024-02-25 00:57:52 +0900

    2024-02-25

--- src/apis/board/boardKeyFactory.js
+++ src/apis/board/boardKeyFactory.js
@@ -2,6 +2,10 @@ export const boardKeyFactory = {
   all: [{ scope: "board" }],
   list: (requestObj) => [{ ...boardKeyFactory.all, ...requestObj }],
   detail: (id) => [{ ...boardKeyFactory.all, id }],
-  comment : [{scope :"comment"}],
-  commentList : (id) => [{ ...boardKeyFactory.comment, id }],
+  comment: [{ scope: "comment" }],
+  commentList: (id) => [{ ...boardKeyFactory.comment, id }],
+  file: [{ scope: "file" }],
+  fileList: (id) => [{ ...boardKeyFactory.file, id }],
+  temp: [{ scope: "temp" }],
+  tempDetail: (id) => [{ ...boardKeyFactory.temp, id }],
 };
