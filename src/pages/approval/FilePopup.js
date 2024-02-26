ddc128814fe42e4956c3ba6de82a421bc2a2bff6
22JunHee <lees8485@gmail.com>
2024-02-26 10:17:02 +0900

    첨부파일 다운로드

--- src/pages/approval/FilePopup.js
+++ src/pages/approval/FilePopup.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './FilePopup.css'; // 팝업에 대한 스타일
 import {callSelectFileAPI} from '../../apis/ApprovalAPICalls'
 import { useSelector, useDispatch } from 'react-redux';
+import { saveAs } from 'file-saver'; // 추가된 부분
 
 
 
@@ -28,12 +29,26 @@ const FilePopup = ({ isOpen, handleClose, content }) => {
     console.log(content,'zzzzzzzzzzzzzzzzzzzzzz')
     if (!isOpen || !content) return null;
 
+    fetch(image.apFileNameChange)
+    .then(response => response.blob())
+    .then(blob => {
+      // 이제 blob 객체를 사용하여 createObjectURL을 호출할 수 있습니다.
+      const url = window.URL.createObjectURL(blob);
+      // ... 나머지 로직
+    })
+    .catch(error => console.error('Error:', error));
+
+
+    const downloadFile = () => {
+      saveAs(image.apFileNameChange, "file"); // 'downloadedImage'는 저장될 파일 이름입니다. 원하는 대로 변경하세요.
+    };
     
   return (
     <div className="popup-overlay1" onClick={handleClose}>
       <div className="popup-container1" onClick={(e) => e.stopPropagation()}>
         {imageContent}
         <button className="popup-close-btn1" onClick={handleClose}>닫기</button>
+        <button className="popup-close-btn1" onClick={downloadFile} >다운로드</button>
       </div>
     </div>
   );
